2d4b05a0162f247a512cab608ed89661909725dd
panox <panospalos@gmail.com>
2015-10-09 15:13:22 +0100

    damage method work

--- js/app.js
+++ js/app.js
@@ -6,15 +6,13 @@ $(function() {
     this.health = health;
     this.damage = function(enemyName){
       var dmg = Math.floor(Math.random() * (maxAtk - minAtk)) + minAtk;
-      console.log(dmg);
-      // enemyName.health -= dmg
-      // return enemyName.health
+      enemyName.health -= dmg;
+      return enemyName.health;
     }
   }
 
   var gary = new Figure('Gary', 100, 3, 10);
   var enemy = new Figure('Darius', 200, 4, 20);
-
 
 
 });
