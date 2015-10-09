e632b5d49660806d536995b3e2d8482943b032b0
panox <panospalos@gmail.com>
2015-10-09 14:58:09 +0100

    object constructor name, health, damage

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(function() {
 
   //Object Constructor
-  function Figure (name, health) {
+  function Figure (name, health, minAtk, maxAtk) {
     this.name = name;
     this.health = health;
+    this.damage = function(enemyName){
+      var dmg = Math.random() * (maxAtk - minAtk) + minAtk;
+      enemyName.health -= dmg
+      return enemyName.health
+    }
   }
 
 
