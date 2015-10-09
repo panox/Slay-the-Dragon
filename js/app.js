e5bf6b2d00ba5a3bbc6c8f06406041b77523ea50
panox <panospalos@gmail.com>
2015-10-09 15:34:55 +0100

    healing method

--- js/app.js
+++ js/app.js
@@ -7,12 +7,19 @@ $(function() {
     this.damage = function(enemyName){
       var dmg = Math.floor(Math.random() * (maxAtk - minAtk)) + minAtk;
       enemyName.health -= dmg;
+      console.log(dmg);
       return enemyName.health;
     }
   }
 
   var gary = new Figure('Gary', 100, 3, 10);
+  gary.healing = function(){
+    var heal = Math.floor(Math.random() * (20 - 4)) + 4;
+    this.health += heal;
+    console.log(heal);
+    return enemyName.health;
+  }
   var enemy = new Figure('Darius', 200, 4, 20);
 
-
+  debugger
 });
