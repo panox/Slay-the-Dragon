64bd351af96c43c3d5918b967bff83c7d3b3b680
panox <panospalos@gmail.com>
2015-10-09 20:31:04 +0100

    enemy health bar

--- js/app.js
+++ js/app.js
@@ -29,6 +29,7 @@ $(function() {
 
   //Health Bars
   progress(gary.health, $('#hero-health'));
+  progress(darius.health/2, $('#enemy-health'));
   function progress(percent, $element) {
       var progressBarWidth = percent * $element.width() / 100;
       $element.find('div').animate({ width: progressBarWidth }, 500).html(percent + "% ");
